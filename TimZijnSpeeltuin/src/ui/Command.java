c40b1f66b147638b6e093d5d63bd35712be158f6
timzijl <timzijl@hotmail.com>
2016-02-11 15:54:30 +0100

    some trying
    octal base

--- TimZijnSpeeltuin/src/ui/Command.java
+++ TimZijnSpeeltuin/src/ui/Command.java
@@ -55,6 +55,7 @@ public class Command {
       else if(command.equals("dec")) calc.setBase(new DecimalBase());
       else if(command.equals("bin")) calc.setBase(new BinaryBase());
       else if(command.equals("hex")) calc.setBase(new HexBase());
+      else if(command.equals("oct")) calc.setBase(new OctalBase());
       else if(command.equals("rat")) calc.setFormat(new RationalFormat());
       else if(command.equals("fixed")) calc.setFormat(new FixedPointFormat());
       else if(command.equals("float")) calc.setFormat(new FloatingPointFormat());
