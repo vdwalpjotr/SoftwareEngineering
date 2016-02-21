8797fda329fcc54c22c30cd2c7d3ecc52c470654
timzijl <timzijl@hotmail.com>
2016-02-21 14:58:30 +0100

    next time do everything at once Tim....

--- TimZijnSpeeltuin3/src/Assignment1/mainClass.java
+++ TimZijnSpeeltuin3/src/Assignment1/mainClass.java
@@ -1,5 +1,25 @@
 package Assignment1;
 
 public class mainClass {
+	
+	public mainClass() {
+		
+		// INORDER
+		current = root; // Begin bij de root
+		while ( current is not NULL or stack is nonempty ) 
+			if ( current is not NULL ){
+				// zelf aanvullen, 2 regels
+			} 
+			else {
+				// zelf aanvullen, 3 regels
+
+			}
+
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
 
 }
