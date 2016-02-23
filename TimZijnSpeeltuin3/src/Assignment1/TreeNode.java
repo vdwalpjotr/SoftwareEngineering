faf7eca360868b84b555c3d9fbe7b7b40f8e4a28
timzijl <timzijl@hotmail.com>
2016-02-23 01:30:26 +0100

    Complete Assigment 1
    InOrder
    PreOrder
    PostOrder

---
+++ TimZijnSpeeltuin3/src/Assignment1/TreeNode.java
@@ -0,0 +1,14 @@
+package Assignment1;
+
+public class TreeNode<E> {
+	
+	protected E element;
+	protected TreeNode<E> left;
+	protected TreeNode<E> right;
+	protected boolean secondPop;
+	
+	public TreeNode(E e) {
+		element = e;
+	}
+
+}
--- TimZijnSpeeltuin3/src/Assignment1/mainClass.java
+++ TimZijnSpeeltuin3/src/Assignment1/mainClass.java
@@ -1,25 +1,73 @@
 package Assignment1;
 
+import java.util.Stack;
+
 public class mainClass {
-	
-	public mainClass() {
-		
-		// INORDER
-		current = root; // Begin bij de root
-		while ( current is not NULL or stack is nonempty ) 
-			if ( current is not NULL ){
-				// zelf aanvullen, 2 regels
-			} 
-			else {
-				// zelf aanvullen, 3 regels
-
-			}
-
-		
-	}
 	
 	public static void main(String[] args) {
 		
 	}
-
+	
+	public void InOrder(BST<String> bst) {		
+		// INORDER
+		TreeNode<String> current = bst.getRoot();
+		Stack<TreeNode<String>> st = new Stack<TreeNode<String>>();
+		
+		while(current != null || !st.empty()) {
+			if(current != null) {
+				st.push(current);
+				current = current.left;
+			} 
+			else {
+				current = st.pop();
+				System.out.println(current.toString());
+				current = current.right;
+			}
+		}
+	}
+	
+	public void PreOrder(BST<String> bst) {
+		// PREORDER
+		TreeNode<String> current = bst.getRoot();
+		Stack<TreeNode<String>> st = new Stack<TreeNode<String>>();
+		
+		while(current != null || !st.empty()) {
+			if(current != null) {
+				System.out.println(current.toString());
+				st.push(current);
+				current = current.left;
+				
+			}
+			else {
+				current = st.pop();
+				current = current.right;
+			}
+		}
+	}
+	
+	public void PostOrder(BST<String> bst) {
+		// POSTORDER
+		TreeNode<String> current = bst.getRoot();
+		Stack<TreeNode<String>> st = new Stack<TreeNode<String>>();
+		
+		while(current != null || !st.empty()) {
+			if(current != null) {
+				st.push(current);
+				current = current.left;
+			} 
+			else {
+				current = st.pop();
+				if(current.secondPop){
+					st.push(current);
+					current = current.right;
+				} 
+				else {
+					System.out.println(current.toString());
+					current.secondPop = true;
+					current = st.pop();
+				}
+			}
+		}
+	}
+	
 }
