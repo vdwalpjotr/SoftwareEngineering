91f43835449ee65bcb8fdcb298fba4623c9ddfc7
timzijl <timzijl@hotmail.com>
2016-02-23 14:23:05 +0100

    Some shit

---
+++ TimZijnSpeeltuin3/src/Assigment2/treeBuild.java
@@ -0,0 +1,26 @@
+package Assigment2;
+
+import java.util.ArrayList;
+
+import javax.swing.JTree;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreeNode;
+
+public class treeBuild {
+	
+	private JTree tree;
+	private DefaultMutableTreeNode dmtn;
+	
+	public treeBuild() {
+		dmtn = new DefaultMutableTreeNode("Assigment2");
+	}
+		
+	public static void main(String[] args) {
+		
+	}
+	
+	private void addNodes() {
+		
+	}
+
+}
