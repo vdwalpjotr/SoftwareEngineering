7fe15ea2056dea963bd2c3754dd14dc5bd907a51
timzijl <timzijl@hotmail.com>
2016-02-25 01:33:30 +0100

    Gaat weer goed

---
+++ TimZijnSpeeltuin3/src/gui/treeGui.java
@@ -0,0 +1,36 @@
+package gui;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+
+public class treeGui extends Application implements EventHandler<ActionEvent> {
+		
+	private questionnaireController qc;
+	protected static int prefHeight = 300;
+	protected static int prefWidth = 300;
+	
+	public void start(Stage primaryStage) {
+		qc = new questionnaireController(prefHeight, prefWidth);
+		Scene scene = new Scene(qc, prefHeight,prefWidth);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Car insurance");
+		primaryStage.show();
+		
+		//primaryStage.setOnCloseRequest(this);
+		
+	}
+
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+
+	@Override
+	public void handle(ActionEvent arg0) {
+		//Alert a = new Alert();
+		
+	}
+}
