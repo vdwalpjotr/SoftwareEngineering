0da896d897d38f000dfa339ea96ab6ebaa84aff3
timzijl <timzijl@hotmail.com>
2016-02-25 21:54:52 +0100

    Leertaak3 cards

---
+++ TimZijnSpeeltuin4/src/cards/Problem.java
@@ -0,0 +1,53 @@
+package cards;
+import java.util.Scanner;
+
+public class Problem
+{
+private Candidates candidates = new Candidates();
+private Solution   solution   = new Solution();
+private Scanner    reader = new Scanner(System.in);
+    
+    public void solve()
+{
+     //System.out.println(candidates);
+     //System.out.println(solution);
+     //reader.nextLine();
+     int index=0;
+     while (index<candidates.size())
+     {
+         if (solution.fits(candidates.get(index)))
+         {
+             solution.record(candidates.remove(index)); //move candidate to solution
+             if (solution.complete())
+             {
+                 solution.show();
+             }
+             else
+             {
+                 solve();
+             }
+             candidates.add(index, solution.eraseRecording()); //move candidate to candidates
+  
+           }
+           index++;
+        }
+}
+    
+    public static void main(String[] arg0) {
+    	Problem p = new Problem();
+    	p.solve();
+    }
+
+}
+        
+          
+         
+
+
+
+
+
+
+
+
+
