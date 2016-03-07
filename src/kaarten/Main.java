777ac0519cb1479cb36fba3be519e4f66911d45b
Peter van der Wal <peterzakelijk@live.nl>
2016-03-07 17:40:36 +0100

    TicTacToe is een kutspel

---
+++ src/kaarten/Main.java
@@ -0,0 +1,9 @@
+package kaarten;
+
+public class Main {
+	public static void main(String[] arg0){
+	Problem problem = new Problem();
+		problem.solve();
+		
+	}
+}
---
+++ src/kaarten/Solution.java
@@ -0,0 +1,161 @@
+package kaarten;
+import java.util.Stack;
+/** the solution is a sequence of cards placed on the board according to the card positions
+    example without border
+*/
+public class Solution extends Stack<Candidate>
+{
+    // The board is an 2D array.
+	// 0123
+	// 0..-.
+	// 1---.
+	// 2.---
+	// 3..-.
+	private Candidate[][] board = new Candidate[4][4];
+	
+	// card positions on the board
+	// the first card position on the board are
+	// {0,2}, {1,0}. {1,1}
+	private int[] row    = { 0, 1, 1, 1, 2, 2, 2, 3 };
+	private int[] column = { 2, 0, 1, 2, 1, 2, 3, 2 };
+	//  indices of adjacent cards in the solution.
+	//                 0   1  2   3   4    5     6    7   
+	int [] [] check = {{},{},{1},{0},{2},{3,4},{5,6},{7}}; 
+	
+	
+	public Solution(){
+	}
+
+	
+	 // Checks whether a candidate with card CardChar is in 
+	 // an adjacent position of the board position (row, column)
+	 // @param row, column, candidate
+	 // @return Boolean indicating if cardChar is found.
+	 // can be used in the methods fits and isCorrect
+	boolean bordersCard(int row, int column, char cardChar){
+	    //TODO
+		Candidate cTop = null;
+		Candidate cBottom = null;
+		Candidate cLeft = null;
+		Candidate cRight = null;
+
+		if(row > 0) {
+			cTop = board[row-1][column];			
+		}
+		if(row < board[0].length - 1) {			
+			cBottom = board[row+1][column];
+		}
+		if(column > 0) {
+			 cLeft = board[row][column-1];
+		}
+		if(column < board[0].length -1) {
+			cRight = board[row][column+1];			
+		}
+		
+		if(cRight != null && cRight.getCardChar() == cardChar) {
+			return false;
+		}
+		else if(cLeft != null && cLeft.getCardChar() == cardChar) {
+			return false;
+		}
+		else if(cTop != null && cTop.getCardChar() == cardChar) {
+			return false;
+		}
+		else if(cBottom != null && cBottom.getCardChar() == cardChar) {
+			return false;
+		}
+		else {
+			return true;
+		}
+    }
+	
+	
+	/**
+	 * Checks whether candidate card of same kind.
+	 * Checks whether by placing candidate the solution sofar still complies with the rules
+	 * @param candidate
+	 * @return boolean indicating whether this candidate can be put in the
+	 * next free position.
+	 */
+	public boolean fits(Candidate candidate){ 
+		//TODO
+		int index = this.size();
+		
+	    return bordersCard(row[index], column[index], candidate.getCardChar());
+		//return true;
+    }
+
+	public void record(Candidate candidate)
+	{
+		int i=this.size(); // i= index in this stack of next for the next candidate
+		board [row[i]] [column[i]] = candidate; //x=row, y=column
+		this.push(candidate);
+		
+	}
+
+	public boolean complete()
+	{
+		return this.size()==8;
+	}
+
+	public void show()
+	{
+		System.out.println(this); 
+	}
+
+	public Candidate eraseRecording()
+	{
+		int i=this.size()-1;           // i= index of the candidate that is removed from this Stack;
+		board[row[i]][column[i]]=null; // remove candidate from board
+		return this.pop();
+    }
+	
+	// can be used in method isCorrect
+    private char mustBeAdjacentTo(char card)
+    {  
+      if (card=='A') return 'K'; 
+      if (card=='K') return 'Q'; 
+      if (card=='Q') return 'J';
+      return '?'; //error
+    }
+	
+	/**
+	 * Checks whether the rules below are fulfilled
+	 * For the positions that can be checked for solution sofar.
+	 * Rules:
+	 * Elke aas (ace) grenst (horizontaal of verticaal) aan een heer (king).
+	 * Elke heer grenst aan een vrouw (queen).
+	 * Elke vrouw grenst aan een boer (jack).
+	 * @return true if all checks are correct.
+	 */
+	// uses methods borderCard and mustBeAdjacent to
+	public boolean isCorrect() {
+         //TODO
+         for(int i = 0; i<row.length; i++) {
+        	 Candidate c = board[row[i]][column[i]];
+        	 char mbat = mustBeAdjacentTo(c.getCardChar());
+
+        	 if((mbat != '?' && c != null) && (bordersCard(row[i], column[i], mbat))) {
+        		 return false;
+        	 }
+         }
+		 return true;
+     } 
+
+	/**
+	 * @return a representation of the solution on the board
+	 */
+     public String toString(){
+	    //TODO
+    	 
+    	 String s = "";
+    	 s = s + "     " + "     " + board[0][2].toString() + "\n";
+    	 s = s + board[1][0].toString() + board[1][1].toString() + board[1][2] + "\n";
+    	 s = s + "     " + board[2][1].toString() + board[2][2].toString() + board[2][3].toString() + "\n";
+    	 s = s + "     " + "     " + board[3][2].toString() + "\n";
+    	 
+    	 
+	    return s;
+	}    
+
+}
