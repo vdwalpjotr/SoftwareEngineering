e55e2422d65851ef37696e9b1cb76f8fa4d30c2f
Peter van der Wal <peterzakelijk@live.nl>
2016-03-09 19:33:11 +0100

    Bottom-up klaar
    Ook werkend voor lege verzameling

--- src/dynProg/DynamischProgrammerenTest.java
+++ src/dynProg/DynamischProgrammerenTest.java
@@ -33,14 +33,16 @@ public class DynamischProgrammerenTest {
 	}
 	
 	private void doTest(){
-		// 3+5+9=17
-		assertTrue( solver.solve( new int[]{3,5,7,9,11}, 17 ));
-		// Lukt niet
 		assertFalse( solver.solve( new int[]{2,4}, 5 ));
+		// 3+5+9=17
+		assertTrue( solver.solve( new int[]{3,5,6,9,11}, 17 ));
+		// Lukt niet
+		
 		// Eén te weinig
 		assertFalse( solver.solve( new int[]{1,1,2,2,3,3,4,4,5,5}, 31 ) );
 		// Precies goed
 		assertTrue( solver.solve( new int[]{1,1,1,2,2,3,3,4,4,5,5}, 31 ) );
+		assertTrue(solver.solve(new int[]{}, 0 ));
 	}
 
 
--- src/dynProg/solvers/BottomUpSolver.java
+++ src/dynProg/solvers/BottomUpSolver.java
@@ -1,10 +1,62 @@
 package dynProg.solvers;
-
 import dynProg.Solver;
 
-public class BottomUpSolver implements Solver{
-	public boolean solve( int[] numbers, int sum){
-		return true;
-	}
 
+public class BottomUpSolver implements Solver {
+
+	public boolean solve(int[] numbers, int sum){
+		//Sum +1, because range from 0 to sum including the 0
+		boolean[][] matrix = new boolean[numbers.length][sum+1];
+		int row = 0;
+		//If sum = 0 and length = 0 or sum=0 return true, if length = 0 but sum is not return false 
+		if(sum == 0){
+			return true;
+		}
+		if(numbers.length == 0){
+			return false;
+		}
+		// loop trough all elements of your collection
+		while( row < numbers.length){
+			matrix[row][numbers[row]] = true;
+			if(row>0){
+				//Having a collection of the same elements can also sum to 2 * that element,
+				//eg {3,3, 5} can have subset sum=3 or sum is 6 or 8 (8 is done in next loop)
+				if(numbers[row-1]==numbers[row] && (2*numbers[row] <= sum)){
+					matrix[row][2*numbers[row]] = true;
+				}
+				//For all indices true on previous row, that indices + current is also true.
+				//eg {1,2,4} will give subsums
+				// 1
+				// 1 , 2, 3,
+				// 1 , 2, 3, 4 , 5, 6, 7
+				for(int i =0; i<sum+1; i++){
+					if(matrix[row-1][i] == true && i != numbers[row]){
+						if( (i+numbers[row]) <= sum) {
+							matrix[row][i+numbers[row]] = true;
+						}
+					}
+				}
+				//If the index on the previous row was true, it must also be true on this row
+				for(int j=0; j<sum+1; j++){					
+					if((matrix[row-1][j] == true)){
+						matrix[row][j] = true;
+					}
+					
+					
+				}
+			}
+			row++;
+}
+
+//		Print matrix
+//		for(int i=0; i<numbers.length;i++){
+//			for(int j=0; j<sum+1; j++){
+//				System.out.print(matrix[i][j]+"|");
+//			}
+//			System.out.print("\n");
+//
+//		}
+
+		return false;
+	}
 }
