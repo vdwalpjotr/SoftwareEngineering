721ddff2dab1f7e543502326c721a9f08d2274c8
timzijl <timzijl@hotmail.com>
2016-03-14 13:44:02 +0100

    Jemoeder
    bullshit

--- TimZijnSpeeltuin5/src/assignmentsLiang/assigment1b.java
+++ TimZijnSpeeltuin5/src/assignmentsLiang/assigment1b.java
@@ -8,22 +8,22 @@ public class assigment1b {
 	public static void main(String[] args) {
 		
 		ExecutorService executor = Executors.newFixedThreadPool(4);
-		executor.execute(new NumberThread(1));
-		executor.execute(new NumberThread(2));
-		executor.execute(new NumberThread(3));
-		executor.execute(new NumberThread(4));
+		executor.execute(new NumberThread2(1));
+		executor.execute(new NumberThread2(2));
+		executor.execute(new NumberThread2(3));
+		executor.execute(new NumberThread2(4));
 		
 		executor.shutdown();
 	}
 
-	public static class NumberThread implements Runnable {
+	public static class NumberThread2 implements Runnable {
 		private static int CURRENT_NUMBER = 4;
 		private int myNumber;
 		
 		private static Lock lock = new ReentrantLock();
 		private static Condition nextNumber = lock.newCondition();
 		
-		public NumberThread(int number) {
+		public NumberThread2(int number) {
 			myNumber = number;			
 		}
 		
